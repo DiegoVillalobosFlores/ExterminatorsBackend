bc8fbb8b78d2fca220c8ad66ac06565e909ff798
Villalobos <diegovillalobos397@gmail.com>
2021-09-15 15:11:53 +0200

    Add .eslintrc.js to enable eslint and add missing fastify dependency

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,15 @@
-require('eslint-config-molindo/setupPlugins');
-
 module.exports = {
-  extends: ['molindo/typescript'],
+  extends: [
+    'airbnb-base',
+  ],
   env: {
+    es2021: true,
     node: true,
-    es6: true,
   },
-  plugins: ['only-ascii'],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
   rules: {
-    // This is sometimes necessary to achive the desired UX
-    'jsx-a11y/no-autofocus': 'off',
-    // Not necessary for Next.js apps
-    'react/react-in-jsx-scope': 'off',
-    // Has some false positives and is not really needed
-    'react/prop-types': 'off',
-    // This is necessary to avoid having characters that might cause some issues
-    'only-ascii/only-ascii': 2,
-    // this rule is deprecated.
-    'jsx-a11y/no-onchange': 'off'
-  }
+  },
 };
