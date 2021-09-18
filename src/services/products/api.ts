a1e03590b2cc2ccde6e2e2865558ca11f1a27b13
Villalobos <diegovillalobos397@gmail.com>
2021-09-19 00:03:16 +0200

    Remove empty line in products/api.ts

--- src/services/products/api.ts
+++ src/services/products/api.ts
@@ -3,7 +3,6 @@ import {Product} from "../../types";
 export default function Api(dbInstance: any) {
   return {
     add: async (product: Product) => {
-
       const result = await dbInstance.add(product);
       if(!result) throw new Error('Unable to add product')
       
